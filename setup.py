7adc9f411e22aa7988a891e9f97f248cf2a553c5
tamton-aquib <aquibjavedt007@gmail.com>
2022-03-11 12:23:19 +0530

    moving from testpypi to pypi as a cli app

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'imreco',
     packages = ['imreco'],
-    version = '0.0.2',
+    version = '0.0.1',
     license = 'MIT',
     description = 'A cli program to resize images and convert between different formats.',
     author = 'aquib',
